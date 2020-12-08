c17a7aa6ff251cf1764cc49972365e8b9153b893
Ismael Francisco Santana Borges <ismaelfranciscosantanaborges@MacBook-Pro-de-Ismael.local>
2020-12-08 05:45:13 -0400

    The map is almost fixed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,8 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:login_flutter/add_experience.dart';
 import 'package:login_flutter/constants.dart';
-import 'package:latlong/latlong.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -75,27 +76,67 @@ class _HomePageState extends State<HomePage> {
                       child: Text('List Empty'),
                     ),
             ),
-            Container(
-              child: FlutterMap(
-                options: new MapOptions(
-                  center: new LatLng(51.5, -0.09),
-                  zoom: 13.0,
-                ),
-                layers: [
-                  new TileLayerOptions(
-                    urlTemplate:
-                        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                    subdomains: ['a', 'b', 'c'],
-                  ),
-                ],
-              ),
-            ),
+            // Container(
+            //   child: FlutterMap(
+            //     options: new MapOptions(
+            //       center: new LatLng(51.5, -0.09),
+            //       zoom: 13.0,
+            //     ),
+            //     layers: [
+            //       new TileLayerOptions(
+            //         urlTemplate:
+            //             "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            //         subdomains: ['a', 'b', 'c'],
+            //       ),
+            //     ],
+            //   ),
+            // ),
+            Map(),
             Container(
               child: Center(child: Text('profile')),
             ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class Map extends StatefulWidget {
+  const Map({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _MapState createState() => _MapState();
+}
+
+class _MapState extends State<Map> {
+  CameraPosition _initialPosition =
+      CameraPosition(target: LatLng(26.2806, 30.8025));
+  Completer<GoogleMapController> _controller = Completer();
+
+  void _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GoogleMap(
+      mapType: MapType.hybrid,
+      initialCameraPosition: _initialPosition,
+      onMapCreated: _onMapCreated,
+      // child: RaisedButton(
+      //   onPressed: () => showLocationPicker(
+      //     context,
+      //     "AIzaSyAIZqCaI4b2JUGrcwall2RTAaNs_xu62dk",
+      //     initialCenter: const LatLng(31.1975844, 29.9598339),
+      //     myLocationButtonEnabled: true,
+      //     layersButtonEnabled: true,
+      //     countries: ['AE', 'NG'],
+
+      //   ),
+      // ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:google_map_location_picker/generated/l10n.dart'
+    as location_picker;
 import 'package:login_flutter/login_page.dart';
 
 void main() => runApp(MyApp());
@@ -9,7 +12,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Login',
+      localizationsDelegates: const [
+        location_picker.S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const <Locale>[
+        Locale('en', ''),
+        Locale('es', ''),
+      ],
       home: LoginPage(),
     );
   }
 }
+//AIzaSyAIZqCaI4b2JUGrcwall2RTAaNs_xu62dk
